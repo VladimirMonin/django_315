461c946472321357c774481cbcf7f9ac68b53d7e
v <v@mail.ru>
2024-06-17 18:17:06 +0500

    lesson_49: Представление категорий

--- blog/urls.py
+++ blog/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from python_blog import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # Добавляем главную страницу
-    path('', views.index),
+    path("", views.index),
+    # Добавляем категории
+    path("category/", views.category),
 ]
--- python_blog/views.py
+++ python_blog/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Привет, Python315!") # вернет страничку с надписью "Привет, мир!" на русском языке.
+    """
+    Представление для главной страницы.
+    """
+    return HttpResponse(
+        """<h1>Мой блог!</h1>
+        <a href="/category/">Категории</a>
+        """
+    )  # вернет страничку с заголовком "Мой блог!" на русском языке.
+
+
+def category(request):
+    """
+    Представление для категорий.
+    """
+    return HttpResponse(
+        """<ul><li>Python</li><li>Django</li><li>Flask</li></ul>
+        <a href="/">На главную</a>
+        """
+    )  # вернет список категорий на русском языке.

9160ffe2ace3b7ad3a8822a327383e57eec10cdc
v <v@mail.ru>
2024-06-26 18:40:45 +0500

    lesson_52: Подключение статики в Django

--- blog/settings.py
+++ blog/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+
+STATICFILES_DIRS = [
+       os.path.join(BASE_DIR, 'static'),  # Путь к статике в корневой директории
+   ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
